3b9dad5a24328cab268acbb0e18a44e156277589
spacemonkey1101 <ritambh54@gmail.com>
2023-01-28 00:01:00 +0530

    cumulative sum approach 4

--- cumSum.js
+++ cumSum.js
@@ -18,8 +18,19 @@ function cumSum3(arr) {
   return arr.reduce((acc,curr) => acc + curr);
 }
 
+function cumSum4(arr){
+  let total = 0 ; 
+
+  for(let i = 0 ; i < arr.length ; i++ ){
+    total += arr[i];
+  }
+
+  return total;
+}
+
 const arr = [1, 3, 4, 5, 5];
 
 console.log(cumSum(arr));
 console.log(cumSum2(arr));
 console.log(cumSum3(arr));
+console.log(cumSum4(arr));
