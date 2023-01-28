243b65fd5dbb628a7a6d43a38e717a6b47d86f64
spacemonkey1101 <ritambh54@gmail.com>
2023-01-28 11:32:27 +0530

    binary search recursive

--- binarySearch.js
+++ binarySearch.js
@@ -1,8 +1,8 @@
-function binaryFunction(arr, key) {
+function binarySearch(arr, key) {
   let s = 0;
   let e = arr.length - 1;
   while (s <= e) {
-    const mid = (s + e) / 2;
+    const mid = Math.floor((s + e) / 2);
     if (arr[mid] == key) {
       return mid;
     } else if (arr[mid] > key) {
@@ -14,4 +14,29 @@ function binaryFunction(arr, key) {
   return -1;
 }
 
-console.log(binaryFunction([1,2,4,6,7], 5))
+function binarySearch2(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+
+  const mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  }
+
+  if (arr[mid] > target) {
+    end = mid - 1;
+  } else {
+    start = mid + 1;
+  }
+
+  return binarySearch(arr, target, start, end);
+}
+
+const arr = [1, 2, 4, 7];
+const arr2 = ["a", "c", "d", "f"];
+console.log(binarySearch(arr, 4));
+console.log(binarySearch(arr2, "d"));
+console.log(binarySearch2(arr, 7));
+console.log(binarySearch2(arr2, "f"));
