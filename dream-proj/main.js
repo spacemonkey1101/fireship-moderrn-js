96fa5bf63bd300f20da476293a826f528c53f853
spacemonkey1101 <ritambh54@gmail.com>
2023-01-31 00:15:29 +0530

    set loading state and put data in the correct place

--- dream-proj/main.js
+++ dream-proj/main.js
@@ -1,22 +1,36 @@
 import "./style.css";
 
 const form = document.querySelector("form");
-const data = new FormData(form);
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const response = await fetch('http://localhost:8080/dream', {
-    method: 'POST',
-    headers : {
-      'Content-Type' : 'application/json'
+  showSpinner();
+  const data = new FormData(form);
+
+  const response = await fetch("http://localhost:8080/dream", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt:data.get('prompt')
-    })
-  })
+      prompt: data.get("prompt"),
+    }),
+  });
+  const { image } = await response.json();
 
-  const image = await response.json();
-
-  const result = document.querySelector('#result');
+  const result = document.querySelector("#result");
   result.innerHTML = `<img src='${image}' width='512' />`;
-  
+  hideSpinner();
 });
+
+function showSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = true;
+  button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>';
+}
+
+function hideSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = false;
+  button.innerHTML = "Dream";
+}
