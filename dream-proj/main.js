2e6d6bc52e8cad90148a3355caf06ca9cadcf5f4
spacemonkey1101 <ritambh54@gmail.com>
2023-01-30 20:43:44 +0530

    frontend UI code

--- dream-proj/main.js
+++ dream-proj/main.js
@@ -1,23 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector("form");
+const data = new FormData(form);
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const response = await fetch('http://localhost:8080/dream', {
+    method: 'POST',
+    headers : {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      prompt:data.get('prompt')
+    })
+  })
 
-setupCounter(document.querySelector('#counter'))
+  const image = await response.json();
+
+  const result = document.querySelector('#result');
+  result.innerHTML = `<img src='${image}' width='512' />`;
+  
+});
